e6c70d379803061fc0bb18a9111fe736d2420751
hubaloleg <hubal.oleg@gmail.com>
2017-01-11 18:57:01 +0200

    screenshot feature

--- app/src/main/java/com/oleg/hubal/accelbase/activity/MainActivity.java
+++ app/src/main/java/com/oleg/hubal/accelbase/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.oleg.hubal.accelbase.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
@@ -21,6 +25,9 @@ import com.oleg.hubal.accelbase.fragment.AccelerometerFragment;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,9 +84,43 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_history:
                 startActivity(new Intent(this, HistoryActivity.class));
                 return true;
+            case R.id.menu_screenshot:
+                takeScreenshot();
+                return true;
             default:
                 return false;
         }
+    }
+
+    private void takeScreenshot() {
+        try {
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/Screenshot_" +  System.currentTimeMillis() + ".jpg";
+
+            View screenView = getWindow().getDecorView().getRootView();
+            screenView.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+            screenView.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
